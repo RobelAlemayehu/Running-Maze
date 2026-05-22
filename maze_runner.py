de040eeb4e854b018bfae1c902a538f16226d710
Hermela <166336343+Hermela-code@users.noreply.github.com>
2026-05-22 08:37:24 +0000

    Implement step generation and wall removal in maze
    Added a function to generate steps in the maze and create cycles by removing random walls.

--- maze_runner.py
+++ maze_runner.py
@@ -25,3 +25,32 @@ path_stack      = []
 dead_ends       = []
 visited_solver  = [[False for _ in range(C)] for _ in range(R)]
 current_pos     = start_node
+def generate_step():
+    global PHASE
+    if not gen_stack:
+        # ADDENDUM: Create cycles by eating 1 in 20 random walls
+        for _ in range(int((R*C)/20)):
+            ir, ic = random.randint(1, R-2), random.randint(1, C-2)
+            if random.random() > 0.5: north_wall[ir][ic] = 0
+            else: east_wall[ir][ic] = 0
+        PHASE = "SOLVING"
+        return
+
+    r, c = gen_stack[-1]
+    gen_visited[r][c] = 1
+    cand = []
+    if r > 0     and not gen_visited[r-1][c]: cand.append((r-1, c))
+    if r < R-1   and not gen_visited[r+1][c]: cand.append((r+1, c))
+    if c > 0     and not gen_visited[r][c-1]: cand.append((r, c-1))
+    if c < C-1   and not gen_visited[r][c+1]: cand.append((r, c+1))
+
+    if cand:
+        nr, nc = random.choice(cand)
+        if nr < r:   north_wall[r][c]    = 0
+        elif nr > r: north_wall[nr][nc]  = 0
+        elif nc < c: east_wall[r][c-1]   = 0
+        elif nc > c: east_wall[r][c]     = 0
+        gen_visited[nr][nc] = 1
+        gen_stack.append((nr, nc))
+    else:
+        gen_stack.pop()
